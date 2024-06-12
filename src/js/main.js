11df7d7e6ef2fa53a6965a53601d2557547fcdde
olivabaeriswyl <122967424+olivabaeriswyl@users.noreply.github.com>
2024-06-12 09:46:03 +0200

    burger menu

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+var burger = document.querySelector(".burger");
+var menu = document.querySelector(".burger-menu");
+
+var toggleMenu = function toggleMenu() {
+  menu.classList.toggle("is-active");
+};
+
+burger.addEventListener("click", toggleMenu);
